401b8fa2da2ceff4227c36f717c5d2ceab681d05
topot <topotru@gmail.com>
2018-03-24 00:19:03 +0300

    exceptions added

---
+++ src/Pheeltrator/Exception.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: topot
+ * Date: 24.03.2018
+ * Time: 0:17
+ */
+
+namespace TopoTrue\Pheeltrator;
+
+
+class Exception extends \Exception
+{
+
+}
---
+++ src/Pheeltrator/Query/Builder/Exception.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: topot
+ * Date: 24.03.2018
+ * Time: 0:17
+ */
+
+namespace TopoTrue\Pheeltrator\Query\Builder;
+
+
+class Exception extends \TopoTrue\Pheeltrator\Exception
+{
+
+}
